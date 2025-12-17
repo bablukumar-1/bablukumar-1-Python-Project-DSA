a20089288441268e3ff30ee1ce1fe615c2ca3957
bablukumar-1 <bablu824130@gmail.com>
2025-12-17 12:50:41 +0530

    i have correct my insertation sort

--- College_DSA/menu.py
+++ College_DSA/menu.py
@@ -221,14 +221,17 @@ def selection_sort():
 # ------------------- Insertion sort ------------------
 def insertion_sort():
     global end_position
-    for i in range(1, end_position+1):
+
+    for i in range(1, end_position):
         pivot = arr[i]
-        for j in range(i - 1, -1, -1):
-            if arr[j] > pivot:
-                arr[j + 1] = arr[j]
-            else:
-                break
-        print(arr)
+        j = i - 1
+
+        while j >= 0 and arr[j] > pivot:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = pivot   # insert pivot at correct place
+        print("insertion sort", arr)
         
 # ---------------- MENU ----------------
 while True:
